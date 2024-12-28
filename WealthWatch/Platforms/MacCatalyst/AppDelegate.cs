116f88618fbd615db24a99cc589d6702bee61101
Sandeep-Thapa-Magar <sandeepthapa520@gmail.com>
2024-12-28 20:55:28 +0545

    Add project files.

---
+++ WealthWatch/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WealthWatch
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
